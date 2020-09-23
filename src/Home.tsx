145e1dc547ec478bdad6107ea8aae2d45fc9c624
unknown <zachary.paul.edwards@gmail.com>
2020-09-23 17:37:53 -0400

    sidebar starting on 'profile-cards'

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,11 +4,29 @@ export default function Home(props: any) {
 
     return (
         <div className="container">
-            <div className="sidebar" contentEditable>
-                This is some content
+            <div className="sidebar">
+                <div className="nav">
+                    <div className="nav-list">
+                        <li>
+                            <a href="">
+                                Artists
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Contact Us
+                            </a>
+                        </li>
+                    </div>
+                </div>
             </div>
             <div className="main" contentEditable>
-                <div className='profile--card'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit reprehenderit animi repudiandae qui maxime necessitatibus doloremque deserunt voluptatem et quo aliquam, minima nesciunt explicabo architecto! Consequatur numquam provident adipisci molestias?</div>
+                <div className='profile--card'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quis quam assumenda iusto iure placeat facilis modi, ea praesentium vero quas. Ipsum quas sapiente libero numquam dicta hic sit perferendis.</div>
                 <div className='profile--card'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quis quam assumenda iusto iure placeat facilis modi, ea praesentium vero quas. Ipsum quas sapiente libero numquam dicta hic sit perferendis.</div>
                 <div className='profile--card'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quasi voluptatem odio saepe sed ipsam repellendus perferendis praesentium doloribus, quaerat aliquam ratione odit incidunt eius nulla sunt at totam eveniet.</div>
                 <div className='profile--card'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio eum suscipit praesentium accusamus. Voluptatibus qui ipsa quam quas neque deserunt, nemo cumque culpa eum modi consequatur, minima officiis non iusto.</div>
