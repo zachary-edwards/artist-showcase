2077fa7aca4df243345bcd532fb10d4297bd6c19
unknown <zachary.paul.edwards@gmail.com>
2020-10-07 08:29:19 -0400

    app changes

--- src/Home.tsx
+++ src/Home.tsx
@@ -52,9 +52,9 @@ export default function Home(props: any) {
                     <div className="profile--content">
                         <div className='profile--card'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quasi voluptatem odio saepe sed ipsam repellendus perferendis praesentium doloribus, quaerat aliquam ratione odit incidunt eius nulla sunt at totam eveniet.</div>
                     </div>
-                    <div className="profile--content">
+                    {/* <div className="profile--content">
                         <div className='profile--card'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quis quam assumenda iusto iure placeat facilis modi, ea praesentium vero quas. Ipsum quas sapiente libero numquam dicta hic sit perferendis.</div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
