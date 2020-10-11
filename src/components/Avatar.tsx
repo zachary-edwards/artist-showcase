513f6f08e1fb72fdffd3950a6765df6274021279
unknown <zachary.paul.edwards@gmail.com>
2020-10-11 17:53:23 -0400

    working on hover for profile content

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -4,6 +4,6 @@ import image from '../assets/kendrick.jpg'
 export default function Avatar (props: any) {
     return (
     <div className={`p-0 avatar`}>
-        <img src={image} alt='profilepicture' style={{width: '157px', height: '100%', borderRadius: '15px'}}/>
+        <img src={image} alt='profilepicture' className='avatar'/>
     </div>)
 }
