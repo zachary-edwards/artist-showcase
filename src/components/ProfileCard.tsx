86f349f9cc107d7a1c698c94942915fcdd6efb4d
unknown <zachary.paul.edwards@gmail.com>
2020-11-24 21:22:20 -0500

    main page layout DONE lol

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -5,11 +5,13 @@ import Bio from './Bio'
 export default function ProfileCard(props: any) {
 
     return (
-        <div className="profile--content">
-            <div className='profile--card'>
-                <Avatar image={props.image}/>
+        <div className="profile--container">
+            <div className="profile--content">
+                <div className='profile--card'>
+                    <Avatar image={props.image} />
 
-                <Bio name={props.name}/>
+                    <Bio name={props.name} />
+                </div>
             </div>
         </div>
     )
