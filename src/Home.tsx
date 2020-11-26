b7196919d53ef572cea90edaa59e39c890171a15
unknown <zachary.paul.edwards@gmail.com>
2020-11-26 09:09:49 -0500

    ruff functionality

--- src/Home.tsx
+++ src/Home.tsx
@@ -85,34 +85,45 @@ export default function Home(props: any) {
             </div>
             <div className="main">
                 <div className="content">
-                <div className="featured--profile">
+                    <div className="featured--profile">
+                        <div className="featured--profile--container">
+                            <div className="profile--card">
+                                <Avatar image={kendrick} />
+                            </div>
+                            <div className="featured--profile-bio">
+                                <div className="featured--profile-header">
+                                    Kendrick Lamar
+                                </div>
+                                <div className="featured--profile-content">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi at modi repellendus dolorum culpa adipisci sed. Adipisci, possimus officiis, provident quasi nemo quaerat dolorem dolor nihil quidem et, accusamus perferendis.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="profiles">
+                        <ProfileCard image={kendrick} name='Kendrick Lamar' />
+                        <ProfileCard image={dababy} name='DaBaby' />
+                        <ProfileCard image={megan} name='Megan Thee Stallion' />
+                        <ProfileCard image={travis} name='Travis Scott' />
 
-                    <Avatar image={kendrick} />
-                </div>
-                <div className="profiles">
-                    <ProfileCard image={kendrick} name='Kendrick Lamar' />
-                    <ProfileCard image={dababy} name='DaBaby' />
-                    <ProfileCard image={megan} name='Megan Thee Stallion' />
-                    <ProfileCard image={travis} name='Travis Scott' />
+                        <ProfileCard image={kendrick} name='Kendrick Lamar' />
+                        <ProfileCard image={dababy} name='DaBaby' />
+                        <ProfileCard image={megan} name='Megan Thee Stallion' />
+                        <ProfileCard image={travis} name='Travis Scott' />
 
-                    <ProfileCard image={kendrick} name='Kendrick Lamar' />
-                    <ProfileCard image={dababy} name='DaBaby' />
-                    <ProfileCard image={megan} name='Megan Thee Stallion' />
-                    <ProfileCard image={travis} name='Travis Scott' />
+                        <ProfileCard image={kendrick} name='Kendrick Lamar' />
+                        <ProfileCard image={dababy} name='DaBaby' />
+                        <ProfileCard image={megan} name='Megan Thee Stallion' />
+                        <ProfileCard image={travis} name='Travis Scott' />
 
-                    <ProfileCard image={kendrick} name='Kendrick Lamar' />
-                    <ProfileCard image={dababy} name='DaBaby' />
-                    <ProfileCard image={megan} name='Megan Thee Stallion' />
-                    <ProfileCard image={travis} name='Travis Scott' />
-
-                    <ProfileCard image={kendrick} name='Kendrick Lamar' />
-                    <ProfileCard image={dababy} name='DaBaby' />
-                    <ProfileCard image={megan} name='Megan Thee Stallion' />
-                    <ProfileCard image={travis} name='Travis Scott' />
-                    {/* <div className="profile--content">
+                        <ProfileCard image={kendrick} name='Kendrick Lamar' />
+                        <ProfileCard image={dababy} name='DaBaby' />
+                        <ProfileCard image={megan} name='Megan Thee Stallion' />
+                        <ProfileCard image={travis} name='Travis Scott' />
+                        {/* <div className="profile--content">
                         <div className='profile--card'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quis quam assumenda iusto iure placeat facilis modi, ea praesentium vero quas. Ipsum quas sapiente libero numquam dicta hic sit perferendis.</div>
                     </div> */}
-                </div>
+                    </div>
                 </div>
             </div>
         </div>
